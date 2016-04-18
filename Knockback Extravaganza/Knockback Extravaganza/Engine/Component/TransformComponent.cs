375136367b655f2f4c9ee3681fba152d5e7cafe0
OSoder <oskar.soder@hotmail.com>
2016-04-18 14:11:35 +0200

    Cleaned MovementSystem

--- Knockback Extravaganza/Knockback Extravaganza/Engine/Component/TransformComponent.cs
+++ Knockback Extravaganza/Knockback Extravaganza/Engine/Component/TransformComponent.cs
@@ -11,7 +11,6 @@ namespace ECS_Engine.Engine.Component {
         public Vector3 Position { get; set; }
         public Vector3 Scale { get; set; }
         public Vector3 Rotation { get; set; }
-
         public Vector3 Forward { get; set; }
         public Vector3 Right { get; set; }
         public Vector3 Up { get; set; }
--- Knockback Extravaganza/Knockback Extravaganza/Engine/Systems/MovementSystem.cs
+++ Knockback Extravaganza/Knockback Extravaganza/Engine/Systems/MovementSystem.cs
@@ -15,79 +15,47 @@ namespace ECS_Engine.Engine.Systems
     {
         public void Update(GameTime gametime, ComponentManager componentManager)
         {
-            //Dictionary<Entity, IComponent> tComponents = componentManager.GetComponents<TransformComponent>();
-            Dictionary<Entity, IComponent> kComponents = componentManager.GetComponents<KeyBoardComponent>();
-            
-            Vector3 changeRotation = new Vector3(0, 0, 0);
-
-            foreach (KeyValuePair<Entity, IComponent> component in kComponents)
-            {
-                //foreach (KeyValuePair<Entity, IComponent> tranformComp in tComponents)
-                KeyBoardComponent keyboardComp = (KeyBoardComponent)component.Value;             
-                TransformComponent tc = componentManager.GetComponent<TransformComponent>(component.Key);          
-                foreach (KeyValuePair<string, BUTTON_STATE> actionState in keyboardComp.ActionStates)
-                {
-                    if ((actionState.Key.Equals("Down") && actionState.Value.Equals(BUTTON_STATE.PRESSED)) || (actionState.Key.Equals("Down") && actionState.Value.Equals(BUTTON_STATE.HELD)))
-                    {
-                        Console.WriteLine("Trycker på W");
-                        tc.Rotation += new Vector3(-.1f, 0, 0);
-                    }
-                    if (actionState.Key.Equals("Up") && actionState.Value.Equals(BUTTON_STATE.PRESSED) || (actionState.Key.Equals("Up") && actionState.Value.Equals(BUTTON_STATE.HELD)))
-                    {
-                        Console.WriteLine("Trycker på S");
-                        tc.Rotation += new Vector3(.1f, 0, 0);
-                    }
-                    if (actionState.Key.Equals("Right") && actionState.Value.Equals(BUTTON_STATE.PRESSED) || (actionState.Key.Equals("Right") && actionState.Value.Equals(BUTTON_STATE.HELD)))
-                    {
-                        Console.WriteLine("Trycker på D");
-                        tc.Rotation += new Vector3(0, -.1f, 0);
-                    }
-                    if (actionState.Key.Equals("Left") && actionState.Value.Equals(BUTTON_STATE.PRESSED) || (actionState.Key.Equals("Left") && actionState.Value.Equals(BUTTON_STATE.HELD)))
-                    {
-                        Console.WriteLine("Trycker på A");
-                        tc.Rotation += new Vector3(0, .1f, 0);
-                    }
-                    if ((actionState.Key.Equals("Move") && actionState.Value.Equals(BUTTON_STATE.PRESSED)) || (actionState.Key.Equals("Move") && actionState.Value.Equals(BUTTON_STATE.HELD)))
-                    {
-                        tc.Position += tc.Forward * (float)gametime.ElapsedGameTime.TotalSeconds * 10;
-                    }
-                }
-            }
+            HandleInput(gametime, componentManager);
 
             //throw new NotImplementedException();
         }
 
         public void HandleInput(GameTime gametime, ComponentManager componentManager)
         {
-            Dictionary<Entity, IComponent> components = componentManager.GetComponents<KeyBoardComponent>();
+            Dictionary<Entity, IComponent> kComponents = componentManager.GetComponents<KeyBoardComponent>();
             Vector3 changeRotation = new Vector3(0, 0, 0);
 
-            foreach (KeyValuePair<Entity, IComponent> component in components)
+            foreach (KeyValuePair<Entity, IComponent> component in kComponents)
             {
                 KeyBoardComponent keyboardComp = (KeyBoardComponent)component.Value;
+                TransformComponent tc = componentManager.GetComponent<TransformComponent>(component.Key);
                 foreach (KeyValuePair<string, BUTTON_STATE> actionState in keyboardComp.ActionStates)
                 {
-                    if (actionState.Key.Equals("Down") && actionState.Value.Equals(BUTTON_STATE.PRESSED))
+                    if ((actionState.Key.Equals("Down") && actionState.Value.Equals(BUTTON_STATE.PRESSED)) || (actionState.Key.Equals("Down") && actionState.Value.Equals(BUTTON_STATE.HELD)))
                     {
-                        Console.WriteLine("Trycker på W");
-                        changeRotation = new Vector3(0, -1, 0);
+                        //Console.WriteLine("Trycker på W");
+                        tc.Rotation += new Vector3(-.1f, 0, 0);
                     }
-                    if (actionState.Key.Equals("Up") && actionState.Value.Equals(BUTTON_STATE.PRESSED))
+                    if (actionState.Key.Equals("Up") && actionState.Value.Equals(BUTTON_STATE.PRESSED) || (actionState.Key.Equals("Up") && actionState.Value.Equals(BUTTON_STATE.HELD)))
                     {
-                        Console.WriteLine("Trycker på S");
-                        changeRotation = new Vector3(0, 1, 0);
+                        //Console.WriteLine("Trycker på S");
+                        tc.Rotation += new Vector3(.1f, 0, 0);
                     }
-                    if (actionState.Key.Equals("Right") && actionState.Value.Equals(BUTTON_STATE.PRESSED))
+                    if (actionState.Key.Equals("Right") && actionState.Value.Equals(BUTTON_STATE.PRESSED) || (actionState.Key.Equals("Right") && actionState.Value.Equals(BUTTON_STATE.HELD)))
                     {
-                        Console.WriteLine("Trycker på D");
-                        changeRotation = new Vector3(-1, 0, 0);
+                        //Console.WriteLine("Trycker på D");
+                        tc.Rotation += new Vector3(0, -.1f, 0);
                     }
-                    if (actionState.Key.Equals("Left") && actionState.Value.Equals(BUTTON_STATE.PRESSED))
+                    if (actionState.Key.Equals("Left") && actionState.Value.Equals(BUTTON_STATE.PRESSED) || (actionState.Key.Equals("Left") && actionState.Value.Equals(BUTTON_STATE.HELD)))
                     {
-                        Console.WriteLine("Trycker på S");
-                        changeRotation = new Vector3(1, 0, 0);
+                        //Console.WriteLine("Trycker på A");
+                        tc.Rotation += new Vector3(0, .1f, 0);
                     }
-
+                    if ((actionState.Key.Equals("Move") && actionState.Value.Equals(BUTTON_STATE.PRESSED)) || (actionState.Key.Equals("Move") && actionState.Value.Equals(BUTTON_STATE.HELD)))
+                    {
+                        //Console.WriteLine("Trycker på Space");
+                        tc.Position += tc.Forward * (float)gametime.ElapsedGameTime.TotalSeconds * 10;
+                    }
                 }
             }
         }
