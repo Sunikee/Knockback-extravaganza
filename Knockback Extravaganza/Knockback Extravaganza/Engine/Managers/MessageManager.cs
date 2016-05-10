b68f5e9ea575386b35967db800977cf9073c84a2
Sunikee <Sunikee@users.noreply.github.com>
2016-05-10 12:49:36 +0200

    Forgot the MessageManager!

---
+++ Knockback Extravaganza/Knockback Extravaganza/Engine/Managers/MessageManager.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS_Engine.Engine.Managers {
+    public class Message {
+        public float acivateInSeconds;
+        public int sender;
+        public int receiver;
+        public string msg;
+    }
+    public class MessageManager {
+
+        private List<Message> messages = new List<Message>();
+        private DateTime startTime;
+        public void Begin(GameTime gametime) {
+            startTime = DateTime.Now;
+            for(int i = 0; i < messages.Count; ++i) {
+                messages[i].acivateInSeconds -= (float)gametime.ElapsedGameTime.TotalSeconds;
+            }
+        }
+        public void End() {
+            messages.RemoveAll(x => x.acivateInSeconds <= 0);
+        }
+        public void RegMessage(Message msg) {
+            messages.Add(msg);
+        }
+        public List<Message> GetMessages(int id) {
+            
+            return messages.FindAll(x => x.receiver == id && x.acivateInSeconds <= 0) as List<Message>;
+        }
+
+    }
+}
