323fb2adfbde2509ec644cf38c92b5e39715c369
limeemil <limeemil@users.noreply.github.com>
2016-06-01 15:04:56 +0200

    Added: The files forgotten in the last commit.

---
+++ Knockback Extravaganza/Knockback Extravaganza/Engine/Component/SceneComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS_Engine.Engine.Component {
+    public class SceneComponent {
+
+        public bool IsActive { get; set; } = false;
+        public bool IsPopup { get; set; }
+        public bool IsBackground { get; set; }
+    }
+}
---
+++ Knockback Extravaganza/Knockback Extravaganza/Engine/Scenes/GameScene.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS_Engine.Engine.Scenes {
+
+    //public enum IsActive{
+    //    Active,
+    //    NonActive
+    //}
+    public abstract class GameScene {
+
+        public bool IsActive { get; set; } = false;
+        public bool IsPopup { get; set; } 
+        public bool IsBackground { get; set; }
+        public List <Entity> entities { get; set; }
+        public string SceneName { get; set; }
+
+
+
+    }
+}
