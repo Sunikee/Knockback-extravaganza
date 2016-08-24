cd55e3a0f08b970d14d3024bd7662a7904b3a7d6
Sunikee <Sunikee@users.noreply.github.com>
2016-08-24 17:23:09 +0200

    Added more network related components and interface

---
+++ Knockback Extravaganza/Knockback Extravaganza/Engine/Network/Interface/INetworkSendComponent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS_Engine.Engine.Network.Interface {
+    class INetworkSendComponent {
+    }
+}
