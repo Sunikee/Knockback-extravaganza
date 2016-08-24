5a89ae8d305dcaf21c5a94aeb2a02db149b6aa53
Sunikee <Sunikee@users.noreply.github.com>
2016-08-24 17:20:22 +0200

    Started the network code implementation.

---
+++ Knockback Extravaganza/Knockback Extravaganza/Engine/Network/Systems/NetworkReceiveSystem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS_Engine.Engine.Systems.Network {
+    class NetworkReceiveSystem {
+    }
+}
